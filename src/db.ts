1582a4195d86e18e6a42976be93854c19321fe86
Patrik Fabijanic <patrik.fabijanic9@gmail.com>
2025-08-12 11:14:32 +0200

    valjda ce sada raditi

--- src/db.ts
+++ src/db.ts
@@ -1,3 +1,21 @@
+<<<<<<< HEAD
+import { MongoClient }from "mongodb"
+import dotenv from "dotenv"
+=======
+<<<<<<< HEAD
+import { MongoClient } from "mongodb"
+import { config } from "dotenv"
+>>>>>>> b10fe56 (valjda ce sada raditi)
+
+dotenv.config()
+
+<<<<<<< HEAD
+const mongoURI = process.env.MONGO_URI as string 
+const database = process.env.MONGO_DB_NAME as string
+=======
+const mongoURI = process.env.MONGO_URI
+const database = process.env.MONGO_DB_NAME
+=======
 import { MongoClient }from "mongodb"
 import dotenv from "dotenv"
 
@@ -5,6 +23,8 @@ dotenv.config()
 
 const mongoURI = process.env.MONGO_URI as string 
 const database = process.env.MONGO_DB_NAME as string
+>>>>>>> 1380466 (valjda ce sada raditi)
+>>>>>>> b10fe56 (valjda ce sada raditi)
 
 if (!mongoURI || !database) {
     throw new Error("MONGO_URI ili MONGO_DB_NAME nisu definirani u .env datoteci");
@@ -13,7 +33,15 @@ if (!mongoURI || !database) {
 
 async function connectToDatabase() {
     try {
+<<<<<<< HEAD
     const client = new MongoClient(mongoURI); 
+=======
+<<<<<<< HEAD
+    const client = new MongoClient(mongoURI!); 
+=======
+    const client = new MongoClient(mongoURI); 
+>>>>>>> 1380466 (valjda ce sada raditi)
+>>>>>>> b10fe56 (valjda ce sada raditi)
     await client.connect();
     console.log('Uspješno spajanje na bazu podataka');
     let db = client.db(database); 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+<<<<<<< HEAD
+import express, { type Express, type Request, type Response} from "express"
+=======
+<<<<<<< HEAD
+import express from "express"
+>>>>>>> b10fe56 (valjda ce sada raditi)
+import cors from "cors"
+import dotenv from "dotenv"
+import userRoutes from "./routes/Users.js"
+
+dotenv.config();
+
+const PORT = process.env.PORT as string;
+
+<<<<<<< HEAD
+const app: Express = express()
+=======
+const app = express()
+=======
 import express, { type Express, type Request, type Response} from "express"
 import cors from "cors"
 import dotenv from "dotenv"
@@ -8,10 +27,13 @@ dotenv.config();
 const PORT = process.env.PORT as string;
 
 const app: Express = express()
+>>>>>>> 1380466 (valjda ce sada raditi)
+>>>>>>> b10fe56 (valjda ce sada raditi)
 
 app.use(cors())
 app.use(express.json())
 
+<<<<<<< HEAD
 app.get('/', (req: Request, res: Response) => {
   res.send('Backend radi!!!')
 })
@@ -19,10 +41,34 @@ app.get('/', (req: Request, res: Response) => {
 app.use("/users", userRoutes);
 
 
+=======
+<<<<<<< HEAD
+app.get('/', (req, res) => {
+  res.send('Backend radi')
+})
+
+=======
+app.get('/', (req: Request, res: Response) => {
+  res.send('Backend radi!!!')
+})
+
+app.use("/users", userRoutes);
+
+
+>>>>>>> 1380466 (valjda ce sada raditi)
+>>>>>>> b10fe56 (valjda ce sada raditi)
 
 app.listen(PORT, error => {
   if (error) {
     console.log('Greška prilikom pokretanja servera', error)
   }
+<<<<<<< HEAD
   console.log(`Aplikacija radi na http://localhost:${PORT}`)
+=======
+<<<<<<< HEAD
+  console.log(`SharpApp poslužitelj dela na http://localhost:${PORT}`)
+=======
+  console.log(`Aplikacija radi na http://localhost:${PORT}`)
+>>>>>>> 1380466 (valjda ce sada raditi)
+>>>>>>> b10fe56 (valjda ce sada raditi)
 })
