951e023f88f2d4a69d54fbd7ffcd847500f11f28
Patrik Fabijanic <patrik.fabijanic9@gmail.com>
2025-08-12 11:26:48 +0200

    ne radi dobro git

--- src/db.ts
+++ src/db.ts
@@ -1,30 +1,13 @@
-<<<<<<< HEAD
-import { MongoClient }from "mongodb"
-import dotenv from "dotenv"
-=======
-<<<<<<< HEAD
 import { MongoClient } from "mongodb"
-import { config } from "dotenv"
->>>>>>> b10fe56 (valjda ce sada raditi)
-
-dotenv.config()
-
-<<<<<<< HEAD
-const mongoURI = process.env.MONGO_URI as string 
-const database = process.env.MONGO_DB_NAME as string
-=======
-const mongoURI = process.env.MONGO_URI
-const database = process.env.MONGO_DB_NAME
-=======
-import { MongoClient }from "mongodb"
 import dotenv from "dotenv"
 
+
 dotenv.config()
 
+
 const mongoURI = process.env.MONGO_URI as string 
-const database = process.env.MONGO_DB_NAME as string
->>>>>>> 1380466 (valjda ce sada raditi)
->>>>>>> b10fe56 (valjda ce sada raditi)
+const database = process.env.MONGO_NAME as string
+
 
 if (!mongoURI || !database) {
     throw new Error("MONGO_URI ili MONGO_DB_NAME nisu definirani u .env datoteci");
@@ -33,15 +16,7 @@ if (!mongoURI || !database) {
 
 async function connectToDatabase() {
     try {
-<<<<<<< HEAD
     const client = new MongoClient(mongoURI); 
-=======
-<<<<<<< HEAD
-    const client = new MongoClient(mongoURI!); 
-=======
-    const client = new MongoClient(mongoURI); 
->>>>>>> 1380466 (valjda ce sada raditi)
->>>>>>> b10fe56 (valjda ce sada raditi)
     await client.connect();
     console.log('Uspješno spajanje na bazu podataka');
     let db = client.db(database); 
