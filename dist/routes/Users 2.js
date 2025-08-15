bce2b16407c908f3d37a827dfcb4fc9cd8f40f53
Patrik Fabijanic <patrik.fabijanic9@gmail.com>
2025-08-15 11:59:51 +0200

    ugasio se komp

---
+++ dist/routes/Users 2.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { db } from "../db.js";
+import { error } from "console";
+const router = express.Router();
+router.get('/', async (req, res) => {
+    let userCollection = db.collection('Users');
+    let allUsers = await userCollection.find().toArray();
+    res.status(200).json(allUsers);
+    if (!allUsers) {
+        res.status(500).json({ msg: "Korisnici nisu dohvaceni", error });
+    }
+});
+export default router;
+//# sourceMappingURL=Users%202.js.map
