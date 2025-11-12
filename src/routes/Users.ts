4d9c7f5ad326519712093a5a8cd0c564a8b9b06f
Patrik Fabijanic <patrik.fabijanic9@gmail.com>
2025-11-12 20:19:46 +0100

    i ovo radi

--- src/routes/Users.ts
+++ src/routes/Users.ts
@@ -1,10 +1,20 @@
 import express, { type Response, type Request } from "express";
 import { connectToDatabase } from "../db.js";
 import User from "../models/User.js";
+import { requireAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-
+router.get("/", requireAuth, async (req, res) => {
+  try {
+    await connectToDatabase();
+    const users = await User.find().select('name lastname email isAdmin');
+    return res.status(200).json(users);
+  } 
+  catch (error) {
+    return res.status(500).json({ msg: 'Error' });
+  }
+});
 
 
 export default router;
