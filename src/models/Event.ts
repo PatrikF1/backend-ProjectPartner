c09ce396a578f433a546d062646d8342c0225d6b
Patrik Fabijanic <patrik.fabijanic9@gmail.com>
2025-11-23 22:12:28 +0100

    dodavanje eventa na kalendar

---
+++ src/models/Event.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IEvent extends Document {
+  title: string;
+  date: Date;
+  description: string;
+  sendAlert: boolean;
+  projectId: mongoose.Types.ObjectId | null;
+  createdBy: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const EventSchema: Schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+  sendAlert: {
+    type: Boolean,
+    default: false,
+  },
+  projectId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Project',
+    default: null,
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, {
+  timestamps: true
+});
+
+export default mongoose.model<IEvent>('Event', EventSchema);
+
