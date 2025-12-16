65bc363a75cd6156ab23f555f23a9966372df7be
Patrik Fabijanic <patrik.fabijanic9@gmail.com>
2025-12-16 22:12:48 +0100

    ne radi dodavanje slike, pre kompleksno za spojiti

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,7 +7,6 @@ export interface IUser extends Document {
   phone?: number;
   passwordHash: string;
   isAdmin: boolean;
-  imageUrl?: string;
 }
 
 const UserSchema: Schema = new Schema({
@@ -39,10 +38,6 @@ const UserSchema: Schema = new Schema({
   isAdmin: {
     type: Boolean,
     default: false
-  },
-  imageUrl: {
-    type: String,
-    required: false
   }
 }, {
   timestamps: true
